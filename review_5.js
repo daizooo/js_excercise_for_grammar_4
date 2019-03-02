91d899cc9962451578f6c7a7b7ecf455e521efe3
overflow326 <s.daizou@outlook.com>
2019-03-02 10:45:16 +0900

    review_5

---
+++ review_5.js
@@ -0,0 +1,18 @@
+function FizzBuzz(i) {
+  if (i < 100) {
+    if (i % 3 === 0 && i % 5 === 0) {
+      console.log(`FizzBuzz`);
+    } else if (i % 3 === 0) {
+      console.log(`Fizz`);
+    } else if (i % 5 === 0) {
+      console.log(`Buzz`);
+    } else {
+      console.log(i);
+    }
+    i++;
+    FizzBuzz(i);
+  }
+  return;
+}
+
+FizzBuzz(1);
